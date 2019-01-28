81bea1de3d7c32b0aa89af25cfb806ecd7f91743
Monisha Aishwarya Vatikuti <monisha.aishwarya.vatikuti@macys.com>
2019-01-28 15:16:33 -0800

    reads cmd line args

--- models/sampleProgram.py
+++ models/sampleProgram.py
@@ -1,5 +1,13 @@
 import time
-sleepTime = 15
+import sys
+
+sleepTime = 5
 print("Sleeping for", sleepTime, "seconds.")
 time.sleep(sleepTime)
 print("Slept for", sleepTime, "seconds.")
+try:
+    input_arg = [sys.argv[1]]
+    print ("Argument:", input_arg)
+except Exception as e:
+    print ("Exception:", e, end=" -- ")
+    print ("No arguments provided")
