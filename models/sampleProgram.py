84f94fd2ec1268411d955c1778e5a629ee9280b9
Monisha Aishwarya Vatikuti <monisha.aishwarya.vatikuti@macys.com>
2019-01-29 10:48:48 -0800

    argv used to create filename as argv_output_file.csv

--- models/sampleProgram.py
+++ models/sampleProgram.py
@@ -5,6 +5,7 @@ sleepTime = 5
 print("Sleeping for", sleepTime, "seconds.")
 time.sleep(sleepTime)
 print("Slept for", sleepTime, "seconds.")
+input_arg = ['NONE']
 try:
     input_arg = [sys.argv[1]]
     print ("Argument:", input_arg)
@@ -20,10 +21,13 @@ print ("Creating DataFrame")
 cols = ['name', 'mean_le', 'lower_bound', 'upper_bound', 'observations',
         'condition1', 'condition2', 'condition3', 'conditions_matched',
         'min_age', 'max_age', 'min_bmi', 'max_bmi', 'lifestyle', 'smoker', 'sex']
-results = [[0000,13.28989579318182,12.5464318524,14.0419858704,100,'overuse or chronic use of prescribed narcotics for pain relief','diabetes','psychosis or psychotic disorder',1,94,102,'','','','','M']]
+results = [[0000,3.28989579318182,2.5464318524,4.0419858704,99,'overuse or chronic use of prescribed narcotics for pain relief','diabetes','psychosis or psychotic disorder',1,94,102,'','','','','M']]
 output_df = pd.DataFrame(data=results, columns=cols)
 
 print ("Writing to file")
-filename = 'output_file.csv'
+
+filename = input_arg[0]+'_output_file.csv'
 output_df.to_csv(filename, index=False)
-print ("Wrote to file", filename) 
+print ("Wrote to file --", filename) 
+
+
