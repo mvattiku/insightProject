fabe869e3edd9a0bd9a5b4e5848e9dec7297427a
Monisha Aishwarya Vatikuti <monisha.aishwarya.vatikuti@macys.com>
2019-03-04 15:37:25 -0800

    removed dummy model and lambda fn used for testing

--- lambdaFn.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-    sourceKey = event['Records'][0]['s3']['object']['key']
-    sourceSize = event['Records'][0]['s3']['object']['size']
-    if (sourceSize != 0):
-        print(event)
-        print("key:", sourceKey)
-    else:
-        print("size: ", sourceSize)
